290dd298c796052b9f32b59962f92242dd6fbf74
Luciano Zablocki <lucianozablocki@gmail.com>
2019-11-23 12:05:26 -0300

    added early stopping

--- prueba2.py
+++ prueba2.py
@@ -16,7 +16,7 @@ import torch.nn.functional as F
 import torch.optim as optim
 import time
 import utils
-#from pytorchtools import EarlyStopping
+from utils import EarlyStopping
 
 class MLP(nn.Module):
     def __init__(self,D_in,H,D_out):
@@ -30,11 +30,11 @@ class MLP(nn.Module):
         #definicion del paso hacia adelante, entra un tensor y devuelve otro tensor 
         h_relu = self.linear1(x).clamp(min=0)
         y_pred = self.linear2(h_relu)
-        #y_pred = F.softmax(self.linear2(h_relu)) #Aplico la softmax 
+        #y_pred = F.softmax(self.linear2(h_relu)) #Aplico la softmax -> no es necesario (incluida en crossentropy)
         
         return y_pred
 
-def prueba2(path_vectors,path_dataset,batch_size,epocas):
+def train_prueba2(path_vectors,path_dataset,batch_size,epocas,patience=None):
     #cargar vectores de embedding
     print("----------CARGANDO VECTORES DE EMBEDDING--------------")
     #wordvectors_file_vec = path_vectors
@@ -59,13 +59,15 @@ def prueba2(path_vectors,path_dataset,batch_size,epocas):
     correctedData = correctedData.values
 
     print("-------------CREANDO INDICES DE TEST Y TRAIN------------------")
-    indxTest,indxTrain = utils.separate_dataset(correctedData,cantidad_preg)
+    indxTest,indxTrain,indxVal = utils.separate_dataset(correctedData,cantidad_preg)
     cant_test = len(indxTest)
     cant_train = len(indxTrain)
+    cant_val = len(indxVal)
     print("cantidad de patrones de prueba: ",cant_test)
     print("cantidad de patrones de entrenamiento: ",cant_train)
+    print("cantidad de patrones de validacion: ",cant_val)
     print("-------------FINISHED CREANDO INDICES DE TEST Y TRAIN------------------\n")
-
+    
     print("-------------CREANDO Ytest e Ytrain (groundtruth de cada subconjunto), Xtext_test y Xtext_train-------------------")
 
     #assert cant_test + cant_train == cantidad_preg 
@@ -76,54 +78,78 @@ def prueba2(path_vectors,path_dataset,batch_size,epocas):
     Ytest = np.zeros((cant_test), dtype=np.int64) #clases de los patrones que estan en el subconjunto de test
     Xtext_test = np.zeros((cant_test,1), dtype=object)
 
+    Yval = np.zeros((cant_val), dtype=np.int64) #clases de los patrones que estan en el subconjunto de validacion
+    Xtext_val = np.zeros((cant_val,1), dtype=object)
+
     contTrain = 0
     contTest = 0
+    contVal = 0
 
     for i in range(cantidad_preg):
-        if i in indxTest:
-            Ytest[contTest] = correctedData[i,0]
-            Xtext_test[contTest] = correctedData[i,1]
-            contTest+=1
-        else:
+        if i in indxTrain:
             Ytrain[contTrain] = correctedData[i,0]
             Xtext_train[contTrain] = correctedData[i,1]
             contTrain+=1
+        elif i in indxTest:
+            Ytest[contTest] = correctedData[i,0]
+            Xtext_test[contTest] = correctedData[i,1]
+            contTest+=1
+        if i in indxVal:
+            Yval[contVal] = correctedData[i,0]
+            Xtext_val[contVal] = correctedData[i,1]
+            contVal+=1
+
 
     Ytrain = torch.from_numpy(Ytrain)
     print("shape del tensor Ytrain : ",Ytrain.shape)
     Ytest = torch.from_numpy(Ytest)
     print("shape del tensor Ytest : ",Ytest.shape)
+    Yval = torch.from_numpy(Yval)
+    print("shape del tensor Yval : ",Yval.shape)
+    print(Xtext_val.shape)
     print("-------------FINISHED CREANDO Ytest, Ytrain, Xtext_test y Xtext_train-------------------\n")
-
+    
     print("--------------CREANDO TENSOR DE PROMEDIOS DE VECTORES DE EMBEDDING VOCABULARIO ORIGINAL PARA ENTRENAMIENTO Y TENSOR DE EMBEDDINGS VOCABULARIO PROPIO----------------------")
     Xtrain,tensorEmbeddingTrain,wordSetTrain = utils.create_tensor_prom_and_embedding(Xtext_train,cant_train,wordvectors)
     print("Size de tensor embedding train: ",tensorEmbeddingTrain.shape)
     print("Size de Xtrain: ", Xtrain.shape)    
     print("--------------FINISHED CREANDO TENSOR DE PROMEDIOS DE VECTORES DE EMBEDDING VOCABULARIO ORIGINAL PARA ENTRENAMIENTO Y TENSOR DE EMBEDDINGS VOCABULARIO PROPIO----------------------\n")
 
+    print("--------------CREANDO TENSOR DE PROMEDIOS DE VECTORES DE EMBEDDING VOCABULARIO ORIGINAL PARA VALIDACION Y TENSOR DE EMBEDDINGS VOCABULARIO PROPIO----------------------")
+    Xval,tensorEmbeddingVal,wordSetVal = utils.create_tensor_prom_and_embedding(Xtext_val,cant_val,wordvectors)
+    print("Size de tensor embedding test: ",tensorEmbeddingVal.shape)
+    print("Size de Xtest: ", Xval.shape)    
+    print("--------------FINISHED CREANDO TENSOR DE PROMEDIOS DE VECTORES DE EMBEDDING VOCABULARIO ORIGINAL PARA VALIDACION Y TENSOR DE EMBEDDINGS VOCABULARIO PROPIO----------------------\n")
+
     print("--------------CREANDO TENSOR DE PROMEDIOS DE VECTORES DE EMBEDDING VOCABULARIO ORIGINAL PARA TEST Y TENSOR DE EMBEDDINGS VOCABULARIO PROPIO----------------------")
     Xtest,tensorEmbeddingTest,wordSetTest = utils.create_tensor_prom_and_embedding(Xtext_test,cant_test,wordvectors)
     print("Size de tensor embedding test: ",tensorEmbeddingTest.shape)
     print("Size de Xtest: ", Xtest.shape)    
     print("--------------FINISHED CREANDO TENSOR DE PROMEDIOS DE VECTORES DE EMBEDDING VOCABULARIO ORIGINAL PARA TEST Y TENSOR DE EMBEDDINGS VOCABULARIO PROPIO----------------------\n")
-
+    
+        
     print("-----------------INIT TRAINING PROCESS---------------------")
     D_in = 300
-    H = 100
+    H = 200
     D_out = cantidad_labels
     #Creo el trainset y el testset
     trainset = torch.utils.data.TensorDataset(Xtrain,Ytrain) 
     testset = torch.utils.data.TensorDataset(Xtest,Ytest)
+    valset = torch.utils.data.TensorDataset(Xval,Yval)
     #Creo el dataloader
     trainloader = torch.utils.data.DataLoader(trainset,batch_size=batch_size,shuffle=True) 
     testloader = torch.utils.data.DataLoader(testset,batch_size=cant_test,shuffle=True)
+    valloader = torch.utils.data.DataLoader(valset,batch_size=cant_val,shuffle=True)
 
     prueba2 = MLP(D_in,H,D_out)
-    criterion = nn.CrossEntropyLoss(reduction = "sum") #analiza reduction
+    criterion = nn.CrossEntropyLoss(reduction = "sum") #analizar reduction
     optimizerPrueba2 = optim.Adam(prueba2.parameters())
-
-    lossVec = [] #vector que guarda el loss para cada epoca
-    ejex = range(epocas) #vector del eje X para graficar
+    early_stopping = EarlyStopping(verbose=True,patience=patience)
+    train_losses = [] #vector que guarda el loss para cada epoca
+    val_losses = []
+    avg_train_losses = []
+    avg_val_losses = []
+    ejex = []
     print("------------- Entrenando un MLP con " , D_in ," neuronas de entrada, " , H , " neuronas en la capa oculta y " , D_out , " neuronas de salida-----------------" )
     for t in range(epocas): 
         for batch_idx, (XX, YY) in enumerate(trainloader):
@@ -132,9 +158,39 @@ def prueba2(path_vectors,path_dataset,batch_size,epocas):
             optimizerPrueba2.zero_grad()
             loss.backward()
             optimizerPrueba2.step()
-        lossVec.append(loss)
-        print("Epoca MLP optimizador ADAM: ", (t,loss.item()))
+            train_losses.append(loss.item())
+
+
+        for (XX,YY) in valloader:
+            out = prueba2(XX)
+            loss = criterion(out,YY)
+            val_losses.append(loss.item())
+
+        train_loss = np.average(train_losses)
+        val_loss = np.average(val_losses)
+        avg_train_losses.append(train_loss)
+        avg_val_losses.append(val_loss)
+        ejex.append(t)
+        epoch_len = len(str(epocas))
         
+        print_msg = (f'[{t:>{epoch_len}}/{epocas:>{epoch_len}}] ' +
+                     f'train_loss: {train_loss:.5f} ' +
+                     f'valid_loss: {val_loss:.5f}')
+        print(print_msg)
+        
+        train_losses = []
+        val_losses = []
+
+        # early_stopping needs the validation loss to check if it has decresed, 
+        # and if it has, it will make a checkpoint of the current model
+        early_stopping(val_loss, prueba2)
+        
+        if early_stopping.early_stop:
+            print("Early stopping")
+            break
+
+    # load the last checkpoint with the best model
+    prueba2.load_state_dict(torch.load('checkpoint.pt'))
     print("-------------------------------FINISHED TRAINING PROCESS-----------------------------\n")
 
     print("--------------------------------INIT TEST PROCESS----------------------------")
@@ -149,14 +205,19 @@ def prueba2(path_vectors,path_dataset,batch_size,epocas):
                 acc+=1
     acc = acc/cant_test*100
     print("------------------------------FINISHED TEST PROCESS-----------------------------------")
-    return ejex,acc,lossVec
+    
+    return ejex,acc,avg_train_losses,avg_val_losses
 
 if __name__ == '__main__':
-    batch_size = 20
+    batch_size = 5
     epocas = 200
     path_vectors = 'C:/Users/lucy/chatbot/fasttext-sbwc.3.6.e20.vec'
     path_dataset = "C:/Users/lucy/chatbot/preprocessedQuestions_lem.csv"
-    ejex,acc,lossVec = prueba2(path_vectors,path_dataset,batch_size,epocas)
-    print(acc)
-    plt.plot(ejex,lossVec)
+    ejex,acc,trainloss,valloss = train_prueba2(path_vectors,path_dataset,batch_size,epocas,patience = 20)
+    # find position of lowest validation loss
+    minposs = valloss.index(min(valloss))+1 
+    plt.axvline(minposs, linestyle='--', color='r',label='Early Stopping Checkpoint')
+    print("Tasa de acierto obtenida: ", acc)
+    plt.plot(ejex,trainloss)
+    plt.plot(ejex,valloss)
     plt.show()
--- utils.py
+++ utils.py
@@ -3,6 +3,50 @@ from nltk import word_tokenize
 import torch 
 
 
+class EarlyStopping:
+    """Early stops the training if validation loss doesn't improve after a given patience."""
+    def __init__(self, patience=7, verbose=False, delta=0):
+        """
+        Args:
+            patience (int): How long to wait after last time validation loss improved.
+                            Default: 7
+            verbose (bool): If True, prints a message for each validation loss improvement. 
+                            Default: False
+            delta (float): Minimum change in the monitored quantity to qualify as an improvement.
+                            Default: 0
+        """
+        self.patience = patience
+        self.verbose = verbose
+        self.counter = 0
+        self.best_score = None
+        self.early_stop = False
+        self.val_loss_min = np.Inf
+        self.delta = delta
+
+    def __call__(self, val_loss, model):
+
+        score = -val_loss
+
+        if self.best_score is None:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+        elif score < self.best_score + self.delta:
+            self.counter += 1
+            print(f'EarlyStopping counter: {self.counter} out of {self.patience}')
+            if self.counter >= self.patience:
+                self.early_stop = True
+        else:
+            self.best_score = score
+            self.save_checkpoint(val_loss, model)
+            self.counter = 0
+
+    def save_checkpoint(self, val_loss, model):
+        '''Saves model when validation loss decrease.'''
+        if self.verbose:
+            print(f'Validation loss decreased ({self.val_loss_min:.6f} --> {val_loss:.6f}).  Saving model ...')
+        torch.save(model.state_dict(), 'checkpoint.pt')
+        self.val_loss_min = val_loss
+
 def contarClase(dataset,clase): #metodo que cuenta la cantidad de patrones en 'dataset' que pertenecen a la clase 'clase'
     contador = 0
     for i in range(dataset.shape[0]):
@@ -49,13 +93,16 @@ def separate_dataset(correctedData,cantidad_preg):
     clase_actual = -1
     indxTest = [] #lista de indices de preguntas en subconjunto de test
     indxTrain= [] #lista de indices de preguntas en subconjunto de train
+    indxVal = []
     for i in range(cantidad_preg):
         if correctedData[i,0]!=clase_actual: #cambio de clase
             clase_actual = correctedData[i,0]
             cantidadPregClase = contarClase(correctedData,clase_actual) #cuento cuantas preguntas hay pertenecientes a la clase actual
             if clase_actual!=46 and clase_actual !=103 and clase_actual!=104 and clase_actual !=105: #clases con solo 1 patron no se incluyen en test
                 indxTest.append(np.random.randint(0, cantidadPregClase-1)+i) #tomo un indice al azar por clase y lo agrego al subconjunto de test
+                indxVal.append(np.random.randint(0, cantidadPregClase-1)+i)
+                #NO ESTA CONTROLADA LA REPETICION DE INDICES EN TEST Y VAL, ATM SE PUEDEN REPETIR
     #Obtengo los indices de train
     indices = range(cantidad_preg)
-    indxTrain = list(filter(lambda x: x not in indxTest, indices)) #al no meter clases con un solo patron al indxTest, pasan automaticamente al indxTrain
-    return indxTest,indxTrain
+    indxTrain = list(filter(lambda x: x not in indxTest and x not in indxVal, indices)) #al no meter clases con un solo patron al indxTest, pasan automaticamente al indxTrain
+    return indxTest,indxTrain,indxVal
