2331397e9c755923ce9e2e2e2f2e9d02b5159ac8
Luciano Zablocki <lucianozablocki@gmail.com>
2019-12-18 20:32:13 -0300

    added variable number of layers for mlp with bow

--- prueba3.py
+++ prueba3.py
@@ -11,8 +11,15 @@ from utils import EarlyStopping
 import bow
 from bow import BOW
 from nltk.corpus import stopwords
-from prueba2 import MLP
-
+#from prueba2 import MLP
+import skorch
+from skorch import NeuralNetClassifier,NeuralNet
+from sklearn.model_selection import RandomizedSearchCV,train_test_split
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import balanced_accuracy_score
+from mpl_toolkits import mplot3d
+import scipy.interpolate as interp
+from mpl_toolkits.mplot3d import Axes3D
 
 class MLP_3ocultas(nn.Module):
     def __init__(self,D_in,H1,H2,H3,D_out):
@@ -38,7 +45,7 @@ class MLP_3ocultas(nn.Module):
 #cargar csv con preguntas preprocesadas
 print("-----------CARGANDO/CREANDO PREGUNTAS PREPROCESADAS--------------")
 #correctedData = preprocesamiento.preprocesar(dataset.values,1) #Dataset lematizado, descomentar en caso de crear otro dataset 
-correctedData = pd.read_csv("C:/Users/lucy/chatbot/preprocessedQuestions_lem.csv",delimiter=',') #comentar esta linea en caso de descomentar la anterior
+correctedData = pd.read_csv("C:/Users/lucy/chatbot/preprocessedQuestions_lem_completadas.csv",delimiter=',') #comentar esta linea en caso de descomentar la anterior
 print("----------FINISHED CARGANDO/CREANDO PREGUNTAS PREPROCESADAS-------------\n")
 
 #obtener datos utiles sobre el dataset
@@ -52,19 +59,26 @@ print("Lista de clases para cada pregunta :",labels)
 correctedData = correctedData.values
 
 print("-------------CREANDO INDICES DE TEST Y TRAIN------------------")
-indxTest,indxTrain,indxVal = utils.separate_dataset(correctedData,cantidad_preg)
-cant_test = len(indxTest)
-cant_train = len(indxTrain)
-cant_val = len(indxVal)
-print("cantidad de patrones de prueba: ",cant_test)
-print("cantidad de patrones de entrenamiento: ",cant_train)
-print("cantidad de patrones de validacion: ",cant_val)
+#indxTest,indxTrain,indxVal = utils.separate_dataset(correctedData,cantidad_preg)
+#cant_test = len(indxTest)
+#cant_train = len(indxTrain)
+#cant_val = len(indxVal)
+#print("cantidad de patrones de prueba: ",cant_test)
+#print("cantidad de patrones de entrenamiento: ",cant_train)
+#print("cantidad de patrones de validacion: ",cant_val)
 print("-------------FINISHED CREANDO INDICES DE TEST Y TRAIN------------------\n")
 stoplist = stopwords.words('spanish')
-bow_unigram = BOW(correctedData,'ascii',stoplist,weighting=True)
+text = correctedData[:,1]
+bow_unigram = BOW(text,'ascii',stoplist,weighting=True)
 
 print("-------------CREANDO Ytest e Ytrain (groundtruth de cada subconjunto), Xtext_test y Xtext_train-------------------")
+Y = np.zeros((cantidad_preg),dtype=np.int64)
+for i in range(cantidad_preg):
+    Y[i] = correctedData[i,0]
 
+Y = torch.from_numpy(Y)
+print(Y)
+"""
 #assert cant_test + cant_train == cantidad_preg 
 #Ytrain = np.zeros((cant_train,1), dtype=np.int64)
 Ytrain = np.zeros((cant_train),dtype=np.int64) #clases de los patrones que estan en el subconjunto de train
@@ -108,16 +122,94 @@ Xval = torch.from_numpy(Xval)
 Xtrain = Xtrain.float()
 Xtest = Xtest.float()
 Xval = Xval.float()
+"""
 print("-------------FINISHED CREANDO Ytest, Ytrain, Xtext_test y Xtext_train-------------------\n")
 
 print("-----------------INIT TRAINING PROCESS---------------------")
+parameters = {    
+'hidden_layer_sizes' : [(200),(100),(200,100),(250,200,100)],
+#'n_layers' : [1,2,3,4],
+#'dropout' : [0.2,0.3,0.4,0.5,0.6],
+#'bidirectional' : [True,False],
+'max_iter' : [1,2,3,4,5],
+'alpha' : [0.00001,0.0001,0.001,0.1,0.2],
+'batch_size' : [100,200,300]
+}
+
 D_in = bow_unigram.X.shape[1]
-H1 = 50
-H2 = 40
-H3 = 30
 D_out = cantidad_labels
+
+net = MLPClassifier(hidden_layer_sizes=(D_in,100,D_out))
+candidatos = 10
+gs = RandomizedSearchCV(net,parameters,verbose=2,n_jobs=-2,cv=2,scoring='balanced_accuracy',n_iter=candidatos)
+X_train,X_test,y_train,y_test = train_test_split(bow_unigram.X,Y,shuffle=True,stratify=Y,test_size=0.1,random_state=12)
+
+gs.fit(X_train,y_train)
+# Utility function to report best scores (found online)
+hidden_layer_sizes = []
+max_epochs = []
+alpha= []
+batch_size=[]
+std=[]
+score=[]
+y_test_tensor = torch.LongTensor(y_test)
+def report(results, n_top=3):
+    for i in range(1, n_top + 1):
+        candidates = np.flatnonzero(results['rank_test_score'] == i)
+        for candidate in candidates:
+            print("Model with rank: {0}".format(i))
+            print("Mean validation score: {0:.3f} (std: {1:.3f})".format(
+                results['mean_test_score'][candidate],
+                results['std_test_score'][candidate]))
+            print("Parameters: {0}".format(results['params'][candidate]))
+            print("")
+            score.append(results['mean_test_score'][candidate])
+            std.append(results['std_test_score'][candidate])
+            hidden_layer_sizes.append(results['params'][candidate]['hidden_layer_sizes'])
+            batch_size.append(results['params'][candidate]['batch_size'])
+            alpha.append(results['params'][candidate]['alpha'])
+            max_epochs.append(results['params'][candidate]['max_iter'])
+
+report(gs.cv_results_,candidatos)  
+
+ejex = batch_size
+ejey = max_epochs
+#print(ejez)
+#print(len(ejez))
+#ejez = [0.5,0.3,0.4,0.33,0.6,0.45,0.75,0.8,0.2,0.47,0.56,0.66,0.9,0.87,0.67,0.43]
+ejez = score
+plotx,ploty, = np.meshgrid(np.linspace(np.min(ejex),np.max(ejex),10),\
+                           np.linspace(np.min(ejey),np.max(ejey),10))
+plotz = interp.griddata((ejex,ejey),ejez,(plotx,ploty),method='linear')
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.plot_surface(plotx,ploty,plotz,cstride=1,rstride=1,cmap='viridis')
+
+#print(X_test.shape)            
+probs = gs.best_estimator_.predict_proba(X_test)
+#print(probs.shape)
+
+# get training and validation loss
+#epochs = [i for i in range(len(gs.best_estimator_.history))]
+epochs = [i for i in range(max_epochs[0])]
+#train_loss = gs.best_estimator_.history[:,'train_loss']
+train_loss = gs.best_estimator_.loss_curve_
+
+#valid_loss = gs.best_estimator_.history[:,'valid_loss']
+acc = balanced_accuracy_score(y_test_tensor,np.argmax(probs,axis=1))
+print("tasa de acierto obtenida: ",acc)
+fig1 = plt.figure()
+plt.plot(epochs,train_loss,'g-')
+#plt.plot(epochs,valid_loss,'r-')
+plt.title('Training Loss Curves')
+plt.xlabel('Epochs')
+plt.ylabel('Cross Entropy Loss')
+plt.legend(['Train','Validation'])
+
+plt.show()
+"""
 #Creo el trainset y el testset
-print(Xtrain.shape)
+#print(Xtrain.shape)
 trainset = torch.utils.data.TensorDataset(Xtrain,Ytrain) 
 testset = torch.utils.data.TensorDataset(Xtest,Ytest)
 valset = torch.utils.data.TensorDataset(Xval,Yval)
@@ -195,3 +287,4 @@ acc = acc/cant_test*100
 print(acc)
 print("------------------------------FINISHED TEST PROCESS-----------------------------------")
 
+"""
