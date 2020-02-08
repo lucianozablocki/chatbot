87d263a1b948727f34f8aa44cf01199af8c30f04
Luciano Zablocki <lucianozablocki@gmail.com>
2020-02-08 19:06:01 -0300

    fixed interface

--- chatbot.py
+++ chatbot.py
@@ -5,6 +5,7 @@ filename = 'C:/Users/lucy/chatbot/SVC_entrenado.sav'
 path_ans = 'C:/Users/lucy/chatbot/respuestas - respuestas.csv'
 path_adjMat = 'C:/Users/lucy/chatbot/adjMat.csv'
 path_quest = 'C:/Users/lucy/chatbot/preprocessedQuestions_stem_completadas.csv'
+path_placeholders =  'C:/Users/lucy/chatbot/placeholders.csv'
 
 #print(len(sys.argv))
 #print(sys.argv)
@@ -12,33 +13,54 @@ R_pd = pd.read_csv(path_ans,delimiter=',',header=None)
 adjMat_pd = pd.read_csv(path_adjMat,delimiter=',',header=None)
 R = R_pd.values
 adjMat = adjMat_pd.values
-#print(adjMat.shape)
+placeholders_pd = pd.read_csv(path_placeholders,delimiter=',',header=None)
+placeholders = placeholders_pd.values
+# print(adjMat.shape)
 actual_node = 109
 loaded_model = pickle.load(open(filename, 'rb'))
 #loaded_bow = pickle.load(open(path_bow,'rb'))
-thres = 0.5
+thres = 0.1
 
 stoplist = stopwords.words('spanish')
 dataset = pd.read_csv(path_quest,header=None)
 bow_unigram = bow.BOW(dataset.values[:,1],'ascii',stoplist,True)
 
-def preprocesar(input):
-    i = quitarStopwordsinput([input])
+keys = placeholders[:,0]
+values = placeholders[:,1]
+replacements = dict(zip(keys,values))
+# print(replacements)
+
+def preprocesar(sentence):
+    # print(f"preprocesar method, input its: {sentence}")
+    i = quitarStopwordsinput(sentence.split()) #we are using whitespaces as separator, potential problem
+    # print(f"output after quitarstopwords and input to limpiarsignos: {i}")
+    #num parameter in split can delimite the number of elements in the resulting list
     i = limpiarSignosinput(i)
-    i = AutocorrectorInput([i])
-    i = Stemmizarinput([i])
-    return i
+    # print(f"output after limpiarsignos and input to autocorrector: {i.split()}")
+    i = AutocorrectorInput(i.split())
+    # print(f"output after autocorrector and input to stemming: {i}")
+    i = Stemmizarinput(i)
+    return i.strip()
 
 while(True):
+    print(f"actual node is: {actual_node}")
+    for placeholder in replacements:
+        R[actual_node][1] = R[actual_node][1].replace(f'<{placeholder}>', replacements[placeholder])
     print(R[actual_node][1])
     i = input()
     pre_input = preprocesar(i)
-    print("after preproc: ", pre_input)
+    print("input after preproc: ", pre_input)
     model_input = bow_unigram.vectorizer.transform([pre_input])
-    probs = loaded_model.predict_proba(model_input) #Esto es de mentirita, estoy prediciendo contra datos de train (chanchan)
-    #print(probs)
+    probs = loaded_model.predict_proba(model_input)
+    print(f"max prob is {np.max(probs)}")
+    # print(probs.shape)
+    # print(np.argmax(probs))
+    # print(probs)
     if(any(x > thres for x in probs[0])):
-        probs_flux = probs*adjMat[actual_node][0:105]
+        if actual_node == 109:
+            probs_flux = probs
+        else:
+            probs_flux = probs*adjMat[actual_node][0:106]
         next_node = np.argmax(probs_flux)
         actual_node = next_node
     elif R[actual_node][2] == 1: #administrative error
