2f43f6db2226a7030ced681aae6f5ecd8a3d0969
Luciano <lucianozablocki@gmail.com>
2020-05-19 16:41:02 -0300

    add flask

--- chatbot.py
+++ chatbot.py
@@ -3,96 +3,104 @@ from preprocesamiento import quitarStopwordsinput,limpiarSignosinput,Autocorrect
 from nltk.corpus import stopwords
 import sklearn
 import datetime
-filename = './SVC_stem_time.sav'
-path_ans = './respuestas - time.csv'
-path_adjMat = './adjMat_time.csv'
-# path_quest = './preprocessedQuestions_stem_completadas.csv'
-path_placeholders =  './placeholders.csv'
-path_bow = "./bow_w_stopwords_time.sav"
 
-print(f'The scikit-learn version is {sklearn.__version__}.')
+class Chatbot:
+    def __init__(self):
+        path_ans = './respuestas - time.csv'
+        path_adjMat = './adjMat_time.csv'
+        path_placeholders =  './placeholders.csv'
+        filename = './SVC_stem_time.sav'
+        path_bow = "./bow_w_stopwords_time.sav"
+        registro = "./registro.txt"
 
-R_pd = pd.read_csv(path_ans,delimiter=',',header=None)
-adjMat_pd = pd.read_csv(path_adjMat,delimiter=',',header=None)
-R = R_pd.values
-adjMat = adjMat_pd.values
-placeholders_pd = pd.read_csv(path_placeholders,delimiter=',',header=None)
-placeholders = placeholders_pd.values
-# print(adjMat.shape)
-actual_node = 116
-loaded_model = pickle.load(open(filename, 'rb'))
-bow_unigram = pickle.load(open(path_bow, 'rb'))
-thres = 0.01
+        R_pd = pd.read_csv(path_ans,delimiter=',',header=None)
+        adjMat_pd = pd.read_csv(path_adjMat,delimiter=',',header=None)
+        placeholders_pd = pd.read_csv(path_placeholders,delimiter=',',header=None)
 
-stoplist = stopwords.words('spanish')
-keys = placeholders[:,0]
-values = placeholders[:,1]
-replacements = dict(zip(keys,values))
+        self.R = R_pd.values
+        self.adjMat = adjMat_pd.values
+        self.placeholders = placeholders_pd.values
+        self.loaded_model = pickle.load(open(filename, 'rb'))
+        self.bow_unigram = pickle.load(open(path_bow, 'rb'))
 
-f = open ("registro.txt", "w")
+        self.thres = 0.01
+        self.actual_node = 116
 
-def preprocesar(sentence):
-    # print(f"preprocesar method, input its: {sentence}")
-    i = quitarStopwordsinput(sentence.split()) #we are using whitespaces as separator, potential problem
-    # print(f"output after quitarstopwords and input to limpiarsignos: {i}")
-    #num parameter in split can delimite the number of elements in the resulting list
-    i = limpiarSignosinput(i)
-    # print(f"output after limpiarsignos and input to autocorrector: {i.split()}")
-    i = AutocorrectorInput(i.split())
-    # print(f"output after autocorrector and input to stemming: {i}")
-    i = Stemmizarinput(i)
-    return i.strip()
+        self.stoplist = stopwords.words('spanish')
+        self.keys = self.placeholders[:,0]
+        self.values = self.placeholders[:,1]
+        self.replacements = dict(zip(self.keys,self.values))
 
-def obtenerNodo(case):
-    switcher = {
-        "bachiller" : 69,
-        "licenciatura" : 107,
-        "curso": 108,
-        "tecnicatura": 109,
-        "posgrado": 110,
-        "reconquista": 88,
-        "galvez": 111,
-        "rafaela": 112
-    }
-
-    return switcher.get(case, 114) #default->error adm
-
-while(True):
-    print(f"actual node is: {actual_node}")
-    for placeholder in replacements:
-        R[actual_node][1] = R[actual_node][1].replace(f'<{placeholder}>', replacements[placeholder])
-    print(R[actual_node][1])
-    f.write(R[actual_node][1] + "\n")
-    if(actual_node==106):
-        print(datetime.datetime.now().strftime("%H:%M:%S")) 
-        f.write(datetime.datetime.now().strftime("%H:%M:%S") + "\n")
-    i = input()
-    f.write(i + "\n")
-    pre_input = preprocesar(i)
-    print("input after preproc: ", pre_input)
-    model_input = bow_unigram.transform([pre_input])
-    probs = loaded_model.predict_proba(model_input)
-    print(f"max prob is {np.max(probs)}")
-    print(f"predicted node is {np.argmax(probs)}")
+        self.f = open (registro, "w")
     
-    if(any(x > thres for x in probs[0])):
-        if actual_node == 116:
-            probs_flux = probs
-        else:
-            probs_flux = probs*adjMat[actual_node][0:107]
-        next_node = np.argmax(probs_flux)
-        actual_node = next_node
-        if actual_node == 69:
-            print("Por favor, decime que carrera queres estudiar")
-            carrera = input()
-            actual_node = obtenerNodo(carrera)
-        elif actual_node == 88:
-            print("Por favor, decime con que sede te queres contactar")
-            sede = input()
-            actual_node = obtenerNodo(sede)
-    elif R[actual_node][2] == 1: #administrative error
-        actual_node = 114
-    elif R[actual_node][2] == 2: #academic error
-        actual_node = 115
-    else: #general error
-        actual_node = 113
+    def preprocesar(self, sentence):
+        # print(f"preprocesar method, input its: {sentence}")
+        i = quitarStopwordsinput(sentence.split()) #we are using whitespaces as separator, potential problem
+        # print(f"output after quitarstopwords and input to limpiarsignos: {i}")
+        #num parameter in split can delimite the number of elements in the resulting list
+        i = limpiarSignosinput(i)
+        # print(f"output after limpiarsignos and input to autocorrector: {i.split()}")
+        i = AutocorrectorInput(i.split())
+        # print(f"output after autocorrector and input to stemming: {i}")
+        i = Stemmizarinput(i)
+        return i.strip()
+
+    def obtenerNodo(self, case):
+        switcher = {
+            "bachiller" : 69,
+            "licenciatura" : 107,
+            "curso": 108,
+            "tecnicatura": 109,
+            "posgrado": 110,
+            "reconquista": 88,
+            "galvez": 111,
+            "rafaela": 112
+        }
+
+        return switcher.get(case, 114) #default->error adm
+
+    def getResponse(self, user_input):
+        previous_answer_type = self.R[self.actual_node][2]
+        self.f.write("user: " + user_input + "\n")
+        pre_input = self.preprocesar(user_input)
+        # print("input after preproc: ", pre_input)
+        model_input = self.bow_unigram.transform([pre_input])
+        probs = self.loaded_model.predict_proba(model_input)
+        # print(f"max prob is {np.max(probs)}")
+        print(f"predicted node is {np.argmax(probs)}")
+        
+        if(any(x > self.thres for x in probs[0])):
+            if self.actual_node == 116:
+                probs_flux = probs
+            else:
+                probs_flux = probs*self.adjMat[self.actual_node][0:107]
+            next_node = np.argmax(probs_flux)
+            self.actual_node = next_node
+            # if self.actual_node == 69:
+            #     print("Por favor, decime que carrera queres estudiar")
+            #     carrera = input()
+            #     self.actual_node = obtenerNodo(carrera)
+            # elif self.actual_node == 88:
+            #     print("Por favor, decime con que sede te queres contactar")
+            #     sede = input()
+            #     self.actual_node = obtenerNodo(sede)
+        elif previous_answer_type == 1: #administrative error
+            self.actual_node = 114
+        elif previous_answer_type == 2: #academic error
+            self.actual_node = 115
+        else: #general error
+            self.actual_node = 113
+        
+        print(f"actual node is: {self.actual_node}")
+        chatbot_response = self.R[self.actual_node][1]
+        for placeholder in self.replacements:
+            chatbot_response = chatbot_response.replace(f'<{placeholder}>', self.replacements[placeholder])
+        # print(self.R[self.actual_node][1])
+        self.f.write("chatbot :" + chatbot_response + "\n")
+        
+        if(self.actual_node==106):
+            # print(datetime.datetime.now().strftime("%H:%M:%S")) 
+            self.f.write(datetime.datetime.now().strftime("%H:%M:%S") + "\n")
+            chatbot_response = chatbot_response + datetime.datetime.now().strftime("%H:%M:%S")
+        self.f.flush()
+        return chatbot_response
---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify, request
+from chatbot import Chatbot
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def get_response():
+    user_input = request.form["input"]
+    return jsonify({"output": chatbot_instance.getResponse(user_input)})
+
+if __name__ == "__main__":
+    chatbot_instance = Chatbot()
+    app.run(debug=True)
